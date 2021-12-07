b552419b9720f511f4c26ea5e225883731ed55fc
Nigel Ritfeld <nigelritfeld@live.nl>
2021-12-07 22:55:05 +0100

    Updated functions

--- src/app/controllers/ProjectController.ts
+++ src/app/controllers/ProjectController.ts
@@ -1,11 +1,22 @@
-export class ProjectController {
+import {Model} from "mongoose";
 
+export class ProjectController {
+    static Project: Model<any> = require('../models/Page.js')
     static showAll(req:any, res:unknown)
     {
         // @ts-ignore
-        res.send('show all projects')
+        this.Project.find((req:any, res:unknown, err:any, projects:any) => {
+            if (err)
+            {
+                // @ts-ignore
+                res.status(500).send(err)
+            }else{
+                // @ts-ignore
+                res.json(projects);
+            }
+            // typeof err !== "undefined" ? res.status(500).send(err) : res.json(projects);
+        })
     }
-
     static show(req:any, res:unknown)
     {
         // @ts-ignore
@@ -13,15 +24,17 @@ export class ProjectController {
     }
     static create(req:any, res:unknown)
     {
+
         // @ts-ignore
-        res.send('Create Project')
+        // this.Project.create()
+        // @ts-ignore
+        res.json(req.body).send()
     }
     static update(req:any, res:unknown)
     {
         // @ts-ignore
         res.send('Update Project')
     }
-
     static remove(req:any, res:unknown)
     {
         // @ts-ignore
