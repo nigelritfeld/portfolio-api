711f16dbac3100258d2be77b6247971f70c773bc
Nigel Ritfeld <nigelritfeld@live.nl>
2021-12-12 00:28:21 +0100

    Fixed production database connect

--- src/Application.ts
+++ src/Application.ts
@@ -10,6 +10,7 @@ const app = express();
 export class Application {
 
     dev: boolean = process.env.NODE_ENV !== 'production';
+    port = process.env.APP === 'production' ? process.env.APP_PORT : 3001;
     sshTunnelConfig: any = this.dev ? {
         // agent: process.env.SSH_AUTH_SOCK,
         user:process.env.TUNNELSSH_USER,
@@ -22,13 +23,17 @@ export class Application {
         localHost: '127.0.0.1',
         localPort: 27017 //or anything else unused you want
     } : null;
-
+    database = this.dev ?  tunnel(this.sshTunnelConfig, (error:any) => {
+        if(error) {
+            console.log("SSH connection error: ", error);
+        }
+        mongoose.connect(`mongodb://${process.env.DEV_DB_USERNAME}:${process.env.DEV_DB_PASSWORD}@127.0.0.1:27017/${process.env.DEV_DB_NAME}`);
+    }): mongoose.connect(`mongodb://${process.env.DEV_DB_USERNAME}:${process.env.DEV_DB_PASSWORD}@127.0.0.1:27017/${process.env.DEV_DB_NAME}`);;
     router = new MainRouter().router
     constructor() {
         app.use(express.json());
         app.use(express.urlencoded({extended: false}));
         app.use('/',this.router)
-        const port = process.env.APP === 'production' ? process.env.APP_PORT : 3001;
         mongoose.connection.on('connecting', () => {
             console.log('connecting')
             console.log(mongoose.connection.readyState); //logs 2
@@ -45,17 +50,10 @@ export class Application {
             console.log('disconnected');
             console.log(mongoose.connection.readyState); //logs 0
         });
-        if (this.dev) {
-            tunnel(this.sshTunnelConfig, (error:any) => {
-                if(error) {
-                    console.log("SSH connection error: ", error);
-                }
-                mongoose.connect(`mongodb://${process.env.DEV_DB_USERNAME}:${process.env.DEV_DB_PASSWORD}@127.0.0.1:27017/${process.env.DEV_DB_NAME}`);
-            });
-        }
-        app.listen(port, () => {
+
+        app.listen(this.port, () => {
             console.log(`Running app in ${process.env.APP}`)
-            console.log(`App listening at http://localhost:${port}`)
+            console.log(`App listening at http://localhost:${this.port}`)
         })
 
     }
