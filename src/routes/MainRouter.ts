d27a5bfe0eee12ccb747a0864c5ff11a82602168
Nigel Ritfeld <nigelritfeld@live.nl>
2021-12-12 00:29:27 +0100

    Updated

--- src/routes/MainRouter.ts
+++ src/routes/MainRouter.ts
@@ -1,12 +1,38 @@
+import {BaseRouter} from "./BaseRouter";
 import {ResourceRouter} from "./ResourceRouter";
+import {CollectionRouter} from "./CollectionRouter";
 import ProjectRouter from "./ProjectRouter";
-import {accept, contentType} from "../app/middlewares/headersMiddleware.js";
+import ProjectCollectionRouter from "./ProjectCollectionRouter";
+const swaggerUi = require('swagger-ui-express');
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            version: "1.0.0",
+            title: "Customer API",
+            description: "Customer API Information",
+            contact: {
+                name: "Amazing Developer"
+            },
+            servers: ["http://localhost:5000"]
+        }
+    },
+    // ['.routes/*.js']
+    apis: ["app.js"]
+};
 
-export class MainRouter extends ResourceRouter{
+
+/**
+ * Main router class
+ */
+export class MainRouter extends BaseRouter{
     constructor() {
         super();
-        this.router.use(accept)
-        this.router.use(contentType)
         this.router.use('/project' , ResourceRouter.routes(ProjectRouter.routeCallbacks))
+        this.router.use('/projects' , CollectionRouter.routes(ProjectCollectionRouter.routeCallbacks))
+        this.router.get('/', function (req, res){
+            res
+                .status(200)
+                .json({home: "ddd"})
+        })
     }
 }
